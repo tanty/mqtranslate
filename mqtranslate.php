a110935b2cc8789fd60031c7e58398f599975f77
Christophe SAUVEUR <christophe@xhaleera.com>
2014-10-26 23:19:11 +0100

    Update mqtranslate.php

--- mqtranslate.php
+++ mqtranslate.php
@@ -51,7 +51,6 @@ Tags: multilingual, multi, language, admin, tinymce, mqTranslate, Polyglot, bili
 	ar by Mohamed Magdy
 	pt by netolazaro and Pedro Mendonça
 	gl by Andrés Bott
-	sr by Borisa Djuraskovic from WebHostingHub <http://www.webhostinghub.com>
 	
 	Plugin Translation Contributers
 	===============================
@@ -82,6 +81,7 @@ Tags: multilingual, multi, language, admin, tinymce, mqTranslate, Polyglot, bili
 	Sponsored Features
 	==================
 	Excerpt Translation by bastiaan van rooden (www.nothing.ch)
+	sr translation by Borisa Djuraskovic from WebHostingHub <http://www.webhostinghub.com>
 
 	Specials thanks
 	===============
